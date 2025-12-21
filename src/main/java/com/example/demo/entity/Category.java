96adc1f8fcb6290459c7891828cc09b05ccb6a8f
bala-amypo <bala@amypo.in>
2025-12-21 17:35:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/Category.java
+++ src/main/java/com/example/demo/entity/Category.java
@@ -2,24 +2,30 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String name;
 
     private String description;
 
     private Boolean active = true;
 
+    // Prevent infinite JSON recursion
+    @JsonIgnore
     @ManyToMany(mappedBy = "categories")
     private Set<MenuItem> menuItems = new HashSet<>();
 }
