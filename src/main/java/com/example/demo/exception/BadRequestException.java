d1d3b01f02d49a81d8de056073b3051f80a8f9fc
bala-amypo <bala@amypo.in>
2025-12-21 16:14:34 +0000

    User commit

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -1,16 +1,8 @@
 package com.example.demo.exception;
 
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
+public class BadRequestException extends RuntimeException {
 
-
- package com.example.demo.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
+    public BadRequestException(String msg) {
         super(msg);
     }
 }
--- src/main/java/com/example/demo/repository/MenuItemRepository.java
+++ src/main/java/com/example/demo/repository/MenuItemRepository.java
@@ -3,5 +3,4 @@ package com.example.demo.repository;
 import com.example.demo.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
-}
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {}
