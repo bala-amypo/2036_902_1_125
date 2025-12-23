a87d3a5854f5f722f3132c3582909da7f0254b30
bala-amypo <bala@amypo.in>
2025-12-23 04:35:48 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,31 +1,26 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
 import java.sql.Timestamp;
 
 @Entity
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
-@Table(name = "users")
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String fullName;
-
-    @Column(unique = true)
     private String email;
-
     private String password;
-
     private String role = "MANAGER";
-
     private Timestamp createdAt;
 
     @PrePersist
-    public void create(){
+    void create() {
         createdAt = new Timestamp(System.currentTimeMillis());
     }
+
+    // getters & setters
 }
