955a18bf746ba03e64888576b50344f3f9b113d6
bala-amypo <bala@amypo.in>
2025-12-23 05:35:46 +0000

    User commit

--- src/main/java/com/example/demo/security/TestJwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/TestJwtAuthenticationFilter.java
@@ -1,8 +1,5 @@
 package com.example.demo.security;
 
-/**
- * Used ONLY for testing / disabling JWT during unit tests
- */
 public class TestJwtAuthenticationFilter {
-    // Intentionally empty
+    
 }
