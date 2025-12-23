6717339a5037d6f05a39b5ba32d8b1aad227e6b3
bala-amypo <bala@amypo.in>
2025-12-23 05:29:47 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -3,6 +3,9 @@ package com.example.demo.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +16,11 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Menu API")
@@ -21,6 +29,13 @@ public class OpenApiConfig {
                 )
                 .servers(List.of(
                         new Server().url("https://9051.408procr.amypo.ai/")
-                ));
+                ))
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth", bearerAuth)
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                );
     }
 }
