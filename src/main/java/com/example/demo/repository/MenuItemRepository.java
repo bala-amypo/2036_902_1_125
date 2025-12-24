d0ff82b7f46be64fcc17e0f703181c44b3a8f8cf
bala-amypo <bala@amypo.in>
2025-12-24 05:22:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/MenuItemRepository.java
+++ src/main/java/com/example/demo/repository/MenuItemRepository.java
@@ -1,12 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.MenuItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import org.springframework.data.jpa.repository.*;
+import java.util.List;
 
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 
-    // REQUIRED BY TESTS
-    Optional<MenuItem> findByNameIgnoreCase(String name);
+    @Query("SELECT DISTINCT m FROM MenuItem m LEFT JOIN FETCH m.categories WHERE m.active = true")
+    List<MenuItem> findAllActiveWithCategories();
 }
