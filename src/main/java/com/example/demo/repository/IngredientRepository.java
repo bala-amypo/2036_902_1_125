8ca972f329459a667be5ee1fa417a18e31996723
bala-amypo <bala@amypo.in>
2025-12-25 08:42:54 +0000

    User commit

--- src/main/java/com/example/demo/repository/IngredientRepository.java
+++ src/main/java/com/example/demo/repository/IngredientRepository.java
@@ -1,11 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Ingredient;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+public interface IngredientRepository extends MenuItemRepository {
 
     Optional<Ingredient> findByNameIgnoreCase(String name);
 }
